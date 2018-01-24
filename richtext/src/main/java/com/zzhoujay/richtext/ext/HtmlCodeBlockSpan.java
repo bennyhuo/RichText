9723abb247c471549947f8d13a44f96210a21a11
enbandari <305779913@qq.com>
2018-01-25 06:08:47 +0800

    添加 折行功能

--- richtext/src/main/java/com/zzhoujay/richtext/ext/HtmlCodeBlockSpan.java
+++ richtext/src/main/java/com/zzhoujay/richtext/ext/HtmlCodeBlockSpan.java
@@ -8,6 +8,9 @@ import android.text.style.LineHeightSpan;
 import android.text.style.ReplacementSpan;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by zhou on 16-7-2.
  * 代码块Span
@@ -38,6 +41,10 @@ public class HtmlCodeBlockSpan extends ReplacementSpan implements LineHeightSpan
     private int start;
     private int end;
 
+    private int singleLineHeight;
+
+    private HashMap<Integer, ArrayList<LineRange>> lineRangesMap = new HashMap();
+
     public HtmlCodeBlockSpan(int width, int backgroundColor, int textColor, int start, int end) {
         mWidth = width;
         this.start = start;
@@ -51,18 +58,21 @@ public class HtmlCodeBlockSpan extends ReplacementSpan implements LineHeightSpan
 
     @Override
     public int getSize(Paint paint, CharSequence text, int start, int end, Paint.FontMetricsInt fm) {
-        Log.w(TAG, "getSize() called with: paint = [" + paint + "], text = [" + text.subSequence(start, end) + "], fm = [" + fm + "]");
+        Log.w(TAG, "getSize() text = [" + text.subSequence(start, end) + "], fm = [" + fm + "]");
         float size = paint.getTextSize();
         paint.setTextSize(size * TEXT_SIZE_SCALE);
-        int width = (int) (paint.measureText(text, start, end) + PADDING * 2);
         paint.setTypeface(Typeface.MONOSPACE);
+
+        measureTextLine(text, start, end, paint);
+
+        Log.d(TAG, lineRangesMap.toString());
         paint.setTextSize(size);
-        return width;
+        return mWidth;
     }
 
     @Override
     public void draw(Canvas canvas, CharSequence text, int start, int end, float x, int top, int y, int bottom, Paint paint) {
-        Log.i(TAG, "draw: canvas = [" + canvas + "], text = [" + text.subSequence(start, end)  + "], x = [" + x + "], top = [" + top + "], y = [" + y + "], bottom = [" + bottom + "], paint = [" + paint + "]");
+        Log.i(TAG, "draw: text = " + text.subSequence(start, end)  + ", x = [" + x + "], top = [" + top + "], y = [" + y + "], bottom = [" + bottom + "]");
         float size = paint.getTextSize();
         int color = paint.getColor();
 
@@ -80,7 +90,10 @@ public class HtmlCodeBlockSpan extends ReplacementSpan implements LineHeightSpan
         }
         mBackground.draw(canvas);
 
-        canvas.drawText(text, start, end, x + PADDING, y + PADDING, paint);
+        ArrayList<LineRange> lineRanges = lineRangesMap.get(start);
+        for (int i = 0; i < lineRanges.size(); i++) {
+            canvas.drawText(text, lineRanges.get(i).start, lineRanges.get(i).end, x + PADDING * 2, y + i * singleLineHeight + PADDING, paint);
+        }
 
         paint.setTextSize(size);
         paint.setColor(color);
@@ -89,6 +102,70 @@ public class HtmlCodeBlockSpan extends ReplacementSpan implements LineHeightSpan
     @Override
     public void chooseHeight(CharSequence text, int start, int end, int spanstartv, int v, Paint.FontMetricsInt fm) {
         Log.e(TAG, "| chooseHeight: text = " + text.subSequence(start, end) + ", spanstartv = [" + spanstartv + "], v = [" + v + "], fm = [" + fm + "]");
+        if(singleLineHeight == 0){
+            singleLineHeight = fm.bottom - fm.top;
+        }
+        fm.bottom = fm.top + singleLineHeight * lineRangesMap.get(start).size();
         fm.descent = fm.bottom + PADDING;
+    }
+
+    private int getTextInLineLen(CharSequence text, int start, int end, Paint paint) {
+        int e = start;
+        while (paint.measureText(text, start, e) < mWidth - PADDING * 2) {
+            e++;
+            if (e > end) {
+                break;
+            }
+        }
+        return e - 1;
+    }
+
+    private int getTextInLineLenInRange(CharSequence text, int start, int end, int rs, int re, Paint paint) {
+        int e = rs;
+        if (rs > end) {
+            return end;
+        }
+        while (paint.measureText(text, start, e) < mWidth - PADDING * 2) {
+            e++;
+            if (e > end || e > re) {
+                break;
+            }
+        }
+        return e - 1;
+    }
+
+    private void measureTextLine(CharSequence text, int start, int end, Paint paint) {
+        int l = getTextInLineLen(text, start, end, paint);
+        int count = l;
+        ArrayList<LineRange> lineRanges = this.lineRangesMap.get(start);
+        if(lineRanges == null){
+            lineRanges = new ArrayList<>();
+            this.lineRangesMap.put(start, lineRanges);
+        }
+        lineRanges.add(new LineRange(start, l));
+        while (l < end) {
+            int temp = l;
+            l = getTextInLineLenInRange(text, l, end, l + count - 4, l + count + 4, paint);
+            count = l - temp;
+            lineRanges.add(new LineRange(temp, l));
+        }
+    }
+
+    static class LineRange{
+        final int start;
+        final int end;
+
+        LineRange(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        public String toString() {
+            return "LineRange{" +
+                    "start=" + start +
+                    ", end=" + end +
+                    '}';
+        }
     }
 }
